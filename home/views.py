dfb94a7ea0f93c9a77858f356e92407c8fef99e9
Alistair Driscoll <pallythecoder@gmail.com>
2025-09-29 11:37:07 +0100

    preliminaries: rearrange basic folders in right place, add changes to settings files, base.html file plus index and correct urls

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """View to show the website homepage"""
+
+    return render(
+        request,
+        "home/index.html",
+    )
