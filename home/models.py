c8002d3d28fbf2e4951c96db8dd52d79c5438fd7
Alistair Driscoll <pallythecoder@gmail.com>
2025-10-02 23:11:55 +0100

    Changes to template and add skillset model

--- home/models.py
+++ home/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+
+class Skill(models.Model):
+    """ Made so the skillset can easily be modified """
+
+    title = models.CharField(max_length=50)
--- home/views.py
+++ home/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from . models import Skill
+
 
 def index(request):
     """View to show the website homepage"""
 
+    skills = Skill.objects.all()
+
+    context = {
+        "skills": skills,
+    }
+
     return render(
         request,
         "home/index.html",
+        context,
     )
