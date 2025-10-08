c2ba840cadce2a0f86d67350b5227e2af3fb678f
Alistair Driscoll <pallythecoder@gmail.com>
2025-10-08 19:41:02 +0100

    add to styling for past work template

--- pastwork/models.py
+++ pastwork/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.text import slugify
 
 
 class CATReview(models.Model):
@@ -21,6 +22,11 @@ class PastWorkAlbum(models.Model):
     """Model where my uncle can upload pictures of previous jobs"""
 
     title = models.CharField(max_length=100)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
