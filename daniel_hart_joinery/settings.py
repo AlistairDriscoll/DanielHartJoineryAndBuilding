8c993be806b2beeb00215f215fcba30b07e9aef2
Alistair Driscoll <pallythecoder@gmail.com>
2025-10-26 15:35:04 +0000

    add dj database url and adjust settings for postgresql

--- daniel_hart_joinery/settings.py
+++ daniel_hart_joinery/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 if os.path.isfile("env.py"):
     import env  # noqa
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,12 +91,16 @@ WSGI_APPLICATION = 'daniel_hart_joinery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if "DATABASE_URL" in os.environ:
+    DATABASES = {"default": dj_database_url.parse(os.environ["DATABASE_URL"])}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+
 
 # Email stuff
 
