bfb03582858b2a5f32243074eb98474e7525ae9e
Your Name <pallythecoder@gmail.com>
2026-02-15 13:53:11 +0000

    Remove Django project for static site

--- enquiries/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.enquiry_page, name="enquiry_page"),
-    path("success/", views.enquiry_success, name="enquiry_success"),
-    path("failure/", views.enquiry_failure, name="enquiry_failure"),
-]
--- pastwork/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PastworkConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pastwork'
